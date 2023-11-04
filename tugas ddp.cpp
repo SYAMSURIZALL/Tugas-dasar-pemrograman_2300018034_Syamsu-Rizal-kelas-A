19c827bd36c1cf4944013415e38efd4e246bf37a
SYAMSURIZALL <149874097+SYAMSURIZALL@users.noreply.github.com>
2023-11-04 19:04:26 +0700

    Tugas Dasar Pemrograman 2300018034 Syamsu Rizal

---
+++ tugas ddp.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i,j,k;
+	for(i=1; j<=1; j++){
+		for(j=1; j<=1; j++){
+			cout<<"___________________________"<<endl;
+			for(k=1; k<=3;k++){
+			cout<<"|______|________|_________|"<<endl;
+		}
+		}
+		
+	}
+			
+	return 0;
+
+}
+
+
+
+
